91f8236a4b9f95e94b866f617736afc0e449a559
Aluno SENAI <Aluno@SN-358658.edu.fiesc.com.br>
2024-07-08 12:06:13 -0300

    adicionando html

--- form.js
+++ form.js
@@ -5,9 +5,14 @@ function enviar(){
    let cpf = document.getElementById('cpf').value
    let tell = document.getElementById('telefone').value
    let email = document.getElementById('email').value
-
+   let idade = document.getElementById('idade').value
    let gnh_mensal = document.querySelector("input[name='ganho_mensal']:checked").value
-
-   alert('nome: '+name+'\ncpf: '+cpf+'\ntelefone: '+tell+ '\nemail: '+ email+ '\nganho mesnsal: '+ gnh_mensal)
    
+   alert('nome: '+name+'\ncpf: '+cpf+'\ntelefone: '+tell+ '\nemail: '+ email+'\nidade: '+idade+'\nganho mesnsal: '+ gnh_mensal)
+   
+   window.location.href='incitel.html'
+} 
+
+function voltar(){
+   window.location.href='form.html'
 } 
